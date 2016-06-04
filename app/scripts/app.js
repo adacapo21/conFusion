822b5b34fb4093edec44ac3b71b9072eabf84800
adacapo21 <aggeloskappos@gmail.com>
2016-06-04 23:50:13 +0300

    contactus completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -53,23 +53,23 @@ angular.module('confusionApp', [])
             $scope.showDetails = !$scope.showDetails;
         };
 
-        this.select = function(setTab) {
-            this.tab = setTab;
+        $scope.select = function(setTab) {
+            $scope.tab = setTab;
             if (setTab === 2) {
-                this.filtText = "appetizer";
+                $scope.filtText = "appetizer";
             }
             else if (setTab === 3) {
-                this.filtText = "mains";
+                $scope.filtText = "mains";
             }
             else if (setTab === 4) {
-                this.filtText = "dessert";
+                $scope.filtText = "dessert";
             }
             else {
-                this.filtText = "";
+                $scope.filtText = "";
             }
         };
-        this.isSelected = function (checkTab) {
-            return (this.tab === checkTab);
+        $scope.isSelected = function (checkTab) {
+            return ($scope.tab === checkTab);
         };
     }])
 
@@ -78,7 +78,12 @@ angular.module('confusionApp', [])
             var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
             $scope.channels = channels;
             $scope.invalidChannelSelection = false;
+
+            //$scope.heroImage = {
+            //    'background-image': 'url(http://www.beathousecollective.com/wp-content/uploads/2016/01/SOLOMUN-beathouse-collective.jpg)'
+            //};
         }])
+
         .controller('FeedbackController', ['$scope', function($scope) {
                 $scope.sendFeedback = function() {
                     console.log($scope.feedback);
@@ -97,6 +102,28 @@ angular.module('confusionApp', [])
                     }
                 };
          }])
+
+    .controller('DishCommentController', ['$scope', function($scope) {
+
+        //Step 1: Create a JavaScript object to hold the comment from the form
+
+        $scope.submitComment = function () {
+            console.log($scope.comment);
+
+            //Step 2: This is how you record the date
+            $scope.comment.date = new Date().toISOString();
+
+            // Step 3: Push your comment into the dish's comment array
+            $scope.dish.comments.push($scope.comment);
+
+            //Step 4: reset your form to pristine
+            $scope.commentForm.$setPristine();
+
+            //Step 5: reset your JavaScript object that holds your comment
+            $scope.comment = {author: "", rating: 5, comment: "", date: new Date().toISOString()};
+            console.log($scope.comment);
+        };
+    }])
 
     .controller('DishDetailController', ['$scope', function($scope) {
 
