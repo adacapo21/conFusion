7ed8ae291548786e8e6e47705f39799d38115e25
adacapo21 <aggeloskappos@gmail.com>
2016-06-14 14:59:09 +0300

    week 3 in progress

---
+++ app/scripts/controllers.js
@@ -0,0 +1,95 @@
+/**
+ * Created by aggeloskappos on 6/14/16.
+ */
+
+'use strict';
+angular.module('confusionApp')
+
+    .controller('MenuController', ['$scope','menuFactory', function($scope, menuFactory) {
+        this.tab = 1;
+        this.filtText = '';
+        $scope.dishes= menuFactory.getDishes();
+
+        $scope.showDetails = false;
+
+        $scope.toggleDetails = function() {
+            $scope.showDetails = !$scope.showDetails;
+        };
+
+        $scope.select = function(setTab) {
+            $scope.tab = setTab;
+            if (setTab === 2) {
+                $scope.filtText = "appetizer";
+            }
+            else if (setTab === 3) {
+                $scope.filtText = "mains";
+            }
+            else if (setTab === 4) {
+                $scope.filtText = "dessert";
+            }
+            else {
+                $scope.filtText = "";
+            }
+        };
+        $scope.isSelected = function (checkTab) {
+            return ($scope.tab === checkTab);
+        };
+    }])
+
+    .controller('ContactController', ['$scope', function($scope) {
+        $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+        var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+        $scope.channels = channels;
+        $scope.invalidChannelSelection = false;
+
+        //$scope.heroImage = {
+        //    'background-image': 'url(http://www.beathousecollective.com/wp-content/uploads/2016/01/SOLOMUN-beathouse-collective.jpg)'
+        //};
+    }])
+
+    .controller('FeedbackController', ['$scope', function($scope) {
+        $scope.sendFeedback = function() {
+            console.log($scope.feedback);
+            if ($scope.feedback.agree && ($scope.feedback.mychannel == "")&& !$scope.feedback.mychannel) {
+                $scope.invalidChannelSelection = true;
+                console.log('incorrect');
+            }
+            else {
+                $scope.invalidChannelSelection = false;
+                $scope.feedback = {mychannel:"", firstName:"", lastName:"",
+                    agree:false, email:"" };
+                $scope.feedback.mychannel="";
+
+                $scope.feedbackForm.$setPristine();
+                console.log($scope.feedback);
+            }
+        };
+    }])
+
+    .controller('DishCommentController', ['$scope', function($scope) {
+
+        //Step 1: Create a JavaScript object to hold the comment from the form
+
+        $scope.submitComment = function () {
+            console.log($scope.comment);
+
+            //Step 2: This is how you record the date
+            $scope.comment.date = new Date().toISOString();
+
+            // Step 3: Push your comment into the dish's comment array
+            $scope.dish.comments.push($scope.comment);
+
+            //Step 4: reset your form to pristine
+            $scope.commentForm.$setPristine();
+
+            //Step 5: reset your JavaScript object that holds your comment
+            $scope.comment = {author: "", rating: 5, comment: "", date: new Date().toISOString()};
+            console.log($scope.comment);
+        };
+    }])
+
+    .controller('DishDetailController', ['$scope','menuFactory', function($scope, menuFactory) {
+
+        $scope.dish= menuFactory.getDish(3);
+
+    }]);
