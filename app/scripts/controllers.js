50e5035a4a957fd9cc2a79a19e9e87797d7b7c1c
adacapo21 <aggeloskappos@gmail.com>
2016-06-26 18:58:23 +0300

    updating controllers 2/7 week 4

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -8,7 +8,13 @@ angular.module('confusionApp')
             $scope.filtText = '';
             $scope.showDetails = false;
 
-            $scope.dishes= menuFactory.getDishes();
+            $scope.dishes= {};
+            menuFactory.getDishes()
+                .then(
+                    function(response) {
+                        $scope.dishes = response.data;
+                    }
+                );
 
                         
             $scope.select = function(setTab) {
@@ -71,8 +77,15 @@ angular.module('confusionApp')
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
             var dish= menuFactory.getDish(parseInt($stateParams.id,10));
-            
-            $scope.dish = dish;
+
+            $scope.dish = {};
+            menuFactory.getDish(parseInt($stateParams.id,10))
+                .then(
+                    function(response){
+                        $scope.dish = response.data;
+                        $scope.showDish=true;
+                    }
+                );
             
         }])
 
@@ -99,7 +112,16 @@ angular.module('confusionApp')
         .controller('IndexController', ['$scope', 'menuFactory', 'corporateFactory', function ($scope, menuFactory, corporateFactory) {
 
             // get random featured dish from array of dishes
-            $scope.featured = menuFactory.getDish(getRandom(menuFactory.getDishes().length));
+            $scope.featured = {};
+
+            menuFactory.getDish(0)
+                .then(
+                    function(response){
+                        $scope.featured = response.data;
+                        $scope.showDish = true;
+                    }
+                );
+            //$scope.featured = menuFactory.getDish(getRandom(menuFactory.getDishes().length));
             $scope.promotion = menuFactory.getPromotion(0);
             $scope.execChef = corporateFactory.getLeader(3);
 
